bd7c0becc467fcdbfee6b4b2bb9042f322d0a66a
Saiteja0506 <saitejabajinku@gmail.com>
2023-06-24 16:57:49 +0530

    'first'

---
+++ cmail.py
@@ -0,0 +1,13 @@
+import smtplib
+from email.message import EmailMessage
+#from smtplib import SMTP_SSL
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('saitejabajinku@gmail.com','hmvznooggxyswtem')
+    msg=EmailMessage()
+    msg['From']='saitejabajinku@gmail.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
